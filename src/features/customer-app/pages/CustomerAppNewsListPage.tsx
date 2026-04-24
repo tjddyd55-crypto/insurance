bcf47f1327cada15a032fc1ac1229d0214a42dce
tjddyd55-crypto <tjddyd55@gmail.com>
2026-04-24 23:55:46 +0900

    fix: import useMemo on customer app news list

--- src/features/customer-app/pages/CustomerAppNewsListPage.tsx
+++ src/features/customer-app/pages/CustomerAppNewsListPage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { StatusMessage } from '../../../components/feedback'
 import { FormButton } from '../../../components/form'
