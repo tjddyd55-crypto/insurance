9fe91a2bfeda320ecb8ea60b948518f675ea001a
tjddyd55-crypto <tjddyd55@gmail.com>
2026-04-25 22:06:29 +0900

    feat: add usage manager to mobile storage view

--- src/features/storage/pages/MyStoragePage/MyStoragePageMobileView.tsx
+++ src/features/storage/pages/MyStoragePage/MyStoragePageMobileView.tsx
@@ -1,7 +1,12 @@
+import StorageUsageManager from '../../components/StorageUsageManager'
 import StorageWorkspace from '../../components/StorageWorkspace'
 import type { MyStorageViewProps } from './myStorageViewProps'
 
-/** 모바일 전용 "내 저장공간" 뷰. PC 뷰와 대칭으로 variant 만 다르게 고정한다. */
 export default function MyStoragePageMobileView(props: MyStorageViewProps) {
-  return <StorageWorkspace {...props} variant="mobile" />
+  return (
+    <>
+      <StorageUsageManager token={props.token} />
+      <StorageWorkspace {...props} variant="mobile" />
+    </>
+  )
 }
