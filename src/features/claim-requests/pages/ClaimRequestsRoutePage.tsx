cb338df05ad094438d5b4861168d93a214f3b758
tjddyd55-crypto <tjddyd55@gmail.com>
2026-04-25 11:08:28 +0900

    refactor: route mobile personal messages through standalone view

--- src/features/claim-requests/pages/ClaimRequestsRoutePage.tsx
+++ src/features/claim-requests/pages/ClaimRequestsRoutePage.tsx
@@ -1,6 +1,7 @@
 import { useSearchParams } from 'react-router-dom'
 import useIsMobile from '../../../hooks/useIsMobile'
 import ClaimRequestsClaimsMobileStandalone from './claim-requests/ClaimRequestsClaimsMobileStandalone'
+import ClaimRequestsPersonalMobileStandalone from './claim-requests/ClaimRequestsPersonalMobileStandalone'
 import ClaimRequestsPage from './ClaimRequestsPage'
 
 export default function ClaimRequestsRoutePage() {
@@ -8,9 +9,14 @@ export default function ClaimRequestsRoutePage() {
   const [searchParams] = useSearchParams()
   const claimTabParam = searchParams.get('claimTab')
   const isClaimsTab = !claimTabParam || claimTabParam === 'claims'
+  const isPersonalTab = claimTabParam === 'news-personal'
 
   if (isMobile && isClaimsTab) {
     return <ClaimRequestsClaimsMobileStandalone />
+  }
+
+  if (isMobile && isPersonalTab) {
+    return <ClaimRequestsPersonalMobileStandalone />
   }
 
   return <ClaimRequestsPage />
